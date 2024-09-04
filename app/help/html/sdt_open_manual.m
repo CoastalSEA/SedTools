808f0e70ac0d2871210b4df7c08900cde14eef1b
CoastalSEA <iantownend@outlook.com>
2024-09-04 16:50:57 +0100

    update maula and example files and rebuild install file

--- app/help/html/sdt_open_manual.m
+++ app/help/html/sdt_open_manual.m
@@ -2,5 +2,6 @@ function sdt_open_manual()
 %find the location of the asmita app and open the manual
 appinfo = matlab.apputil.getInstalledAppInfo;
 idx = find(strcmp({appinfo.name},'SedTools'));
-fpath = [appinfo(idx(1)).location,[filesep,'SedTools',filesep,'doc',filesep,'SedTools manual.pdf']];
+fpath = [appinfo(idx(1)).location,[filesep,'SedTools',filesep,'app',filesep,...
+                                 'doc',filesep,'SedTools manual.pdf']];
 open(fpath)
